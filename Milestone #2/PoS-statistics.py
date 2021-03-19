66cec146e9fb0be77dd1c1cb376014d269e46145
Kevin Starmack <70070214+kevinstarmack@users.noreply.github.com>
2021-03-19 13:06:36 -0400

    Create PoS-statistics.py

---
+++ Milestone #2/PoS-statistics.py
@@ -0,0 +1,17 @@
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+from nltk.tokenize import word_tokenizeinput_file = open('req-parse.txt').read()
+type(input_file)text = input_file #where input file is any input file
+print()
+print("----------------------------------------------------------------")
+print("This tokenization prints all words including punctuation marks")
+print("----------------------------------------------------------------")
+words_from_sentence = word_tokenize(text)
+print(word_tokenize(text))
+print()print()
+print("---------------------------------------------------------")
+print("This prints all words with associated part of speech")
+print("---------------------------------------------------------")
+parts_of_speech = nltk.pos_tag(words_from_sentence)
+print(parts_of_speech)
