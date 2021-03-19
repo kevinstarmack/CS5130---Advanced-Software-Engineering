45455b453c84ac61128f99b1a1941aa39274f7a5
Kevin Starmack <70070214+kevinstarmack@users.noreply.github.com>
2021-03-19 13:09:25 -0400

    Update PoS-statistics.py
    psuedocode added

--- Milestone #2/PoS-statistics.py
+++ Milestone #2/PoS-statistics.py
@@ -15,3 +15,6 @@ print("This prints all words with associated part of speech")
 print("---------------------------------------------------------")
 parts_of_speech = nltk.pos_tag(words_from_sentence)
 print(parts_of_speech)
+
+# insert statistics from feedback from the output of the initial code
+# using both the PoS statistics and the classifier, we can build a confusion matrix
